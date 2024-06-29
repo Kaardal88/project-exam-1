a0cbc74fd724ae97c4871cf6eea59021e1f9eb4d
Kaardal88 <123365145+Kaardal88@users.noreply.github.com>
2024-06-29 14:46:00 +0200

    All

--- js/api/api.js
+++ js/api/api.js
@@ -1,1 +1,2 @@
  export const api = "https://kimkar88.no/wp-json/wp/v2/posts";
+ export const mediaApi = "https://www.kimkar88.no/wp-json/wp/v2/media";
--- js/blogposts.js
+++ js/blogposts.js
@@ -1,5 +1,6 @@
 import { api } from "./api/api.js";
 import { blogPostElement } from "./constants/constants.js";
+import { mediaApi } from "./api/api.js";
 
 async function getPosts() {
     try {
@@ -20,22 +21,38 @@ async function getPosts() {
     }
 }
 
-function createHTML(posts) {
-    posts.forEach(function(post) {
+async function getThumbnail(mediaId) {
+    try {
+        const response = await fetch(`${mediaApi}/${mediaId}`);
+        if (!response.ok) {
+            throw new Error('Error fetching thumbnail');
+        }
+        const media = await response.json();
+        return media.source_url; // Adjust this according to your API response
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
+async function createHTML(posts) {
+    blogPostElement.className = "posts-content";
+    for (const post of posts) {
+        const thumbnailUrl = await getThumbnail(post.featured_media);
         const div = document.createElement('div');
-        blogPostElement.className = "posts-content";
         div.innerHTML = `<div>
                             <h3>
-                                <a href="/blogspecific.html?id=${post.id}&title=${post.title.rendered}"> 
-                                <p class="blogpost-infotext">${post.title.rendered}</p>
+                                <a href="/blogspecific.html?id=${post.id}&title=${post.title}">
+                                    ${post.title.rendered}
                                 </a>
                             </h3>
+                            <img src="${thumbnailUrl}" alt="Thumbnail">
+                            <p class="blogpost-infotext">${post.excerpt.rendered}</p>
                         </div>`;
-        blogPostElement.appendChild(div); 
-    });
+        blogPostElement.appendChild(div);
+    }
 }
 
 getPosts();
-
 
 
