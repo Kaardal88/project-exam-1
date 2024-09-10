e8bfa9d99c9f6898ea5087b89b854ddefd695bb9
Kaardal88 <123365145+Kaardal88@users.noreply.github.com>
2024-09-10 21:35:22 +0200

    removed show info on scroll. Adjusting info on index. Implemented blury backdrop when mobilenav is open.

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,14 +1,23 @@
-
-
 const menuButton = document.querySelector('#menu-button')
 const mobileNavList = document.querySelector('#mobile-nav-list')
+const blury = document.getElementById('blury')
 
 menuButton.addEventListener('click', () => {
     mobileNavList.classList.toggle('hide');
-})
+
+    if (mobileNavList.classList.contains('hide')) {
+        menuButton.textContent = '☰';
+        blury.classList.remove('blur');
+    } else {
+        menuButton.textContent = '✖';
+        blury.classList.toggle('blur')
+    }
+});
 
 document.addEventListener('click', e => {
     if (!mobileNavList.contains(e.target) && e.target !== menuButton) {
-        mobileNavList.classList.add('hide')
+        mobileNavList.classList.add('hide');
+        menuButton.textContent = '☰';
     }
-})
+});
+
